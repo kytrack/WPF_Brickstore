9fce27575eeca3e0d2d264a1e8a129ff4747acbe
kytrack <70961365+kytrack@users.noreply.github.com>
2024-09-09 09:46:08 +0200

    .
    .

---
+++ WPF_Brickstore/WPF_Brickstore/LegoClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Brickstore
+{
+    internal class LegoClass
+    {
+        private string itemID;
+        private string itemName;
+        private string categoryName;
+        private string colorName;
+        private int qty;
+
+        public LegoClass(string itemID, string itemName, string categoryName, string colorName, int qty)
+        {
+            this.itemID = itemID;
+            this.itemName = itemName;
+            this.categoryName = categoryName;
+            this.colorName = colorName;
+            this.qty = qty;
+        }
+
+        public string ItemID { get { return itemID; } }
+        public string ItemName { get { return itemName; } }
+        public string CategoryName { get {return categoryName; } }
+        public string ColorName { get {return colorName;} }
+        public int Qty { get {return qty;} }
+    }
+}
---
+++ WPF_Brickstore/WPF_Brickstore/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using Microsoft.Win32;
+
+namespace WPF_Brickstore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string kivalasztottFajl = "";
+        List<LegoClass> legoItems = new List<LegoClass>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            fajlValasztas();
+        }
+
+        private void btnSzukit_Click(object sender, RoutedEventArgs e)
+        {
+            Szures();
+        }
+        private void fajlValasztas()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "BSX files (*.bsx)|*.bsx";
+            bool? result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                XDocument xaml = XDocument.Load(selectedFilePath);
+                foreach (var elem in xaml.Descendants("Item"))
+                {
+                    string id = elem.Element("ItemID").Value;
+                    string nev = elem.Element("ItemName").Value;
+                    string kategoria = elem.Element("CategoryName").Value;
+                    string szin = elem.Element("ColorName").Value;
+                    int qty = Convert.ToInt32(elem.Element("Qty").Value);
+
+                    legoItems.Add(new LegoClass(id, nev, kategoria, szin, qty));
+                }
+                dgElemek.ItemsSource = legoItems;
+            }
+            else
+            {
+                fajlValasztas();
+            }
+        }
+        private void Szures()
+        {
+            string keresettNev = txtNev.Text.ToLower();
+            string keresettAzon = txtAzon.Text.ToLower();
+
+            var szurtLista = legoItems.Where(item =>
+                (string.IsNullOrEmpty(keresettNev) || item.ItemName.ToLower().StartsWith(keresettNev)) &&
+                (string.IsNullOrEmpty(keresettAzon) || item.ItemID.ToString().ToLower().StartsWith(keresettAzon))
+            ).ToList();
+
+            dgElemek.ItemsSource = szurtLista;
+        }
+
+    }
+}
